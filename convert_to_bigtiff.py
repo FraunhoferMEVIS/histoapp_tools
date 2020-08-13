c00183cf7522663485e0c6be66678365c523194f
Johannes Lotz <johannes.lotz@mevis.fraunhofer.de>
2020-08-13 17:30:00 +0200

    adding pixels-size override and some output to converter

--- convert_to_bigtiff.py
+++ convert_to_bigtiff.py
@@ -12,8 +12,14 @@ def main():
         print("Usage: convert_to_bigtiff.py [input file] [output file]")
     else:
         image = pyvips.Image.new_from_file(sys.argv[1], access='random')
-        print(image.xres)
-        image.tiffsave(sys.argv[2], xres=image.xres, yres=image.yres, bigtiff=True,pyramid=True,tile=True,compression="jpeg",rgbjpeg=True)
+        xres = image.xres
+        yres = image.yres
+        print("Converting {} with pixel size {}x{}.".format(sys.argv[1],xres,yres))
+        if xres == 0 or yres == 0:
+            xres=4000
+            yres=4000
+            print("... setting pixel size to {}x{}.".format(xres,yres))
+        image.tiffsave(sys.argv[2], xres=xres, yres=yres, bigtiff=True,pyramid=True,tile=True,compression="jpeg",rgbjpeg=True)
 
 if __name__ == "__main__":
     main()
