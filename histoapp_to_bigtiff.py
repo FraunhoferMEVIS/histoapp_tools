8b4cc007cb572045e738d805ce94b7c0f0444cf9
Johannes Lotz <johannes.lotz@mevis.fraunhofer.de>
2022-02-23 07:56:08 +0100

    bugfixes in bigtiff downloader

--- histoapp_to_bigtiff.py
+++ histoapp_to_bigtiff.py
@@ -5,6 +5,7 @@
 # Downloads image patches from a webserver and sequentially writes them into a bigtiff file.
 # Note that libvips is required and can be installed using "apt install libvips" (Ubuntu) or "brew install vips" (Mac OS)
 
+from curses import meta
 import io
 import json
 
@@ -13,10 +14,12 @@ import pyvips
 import requests
 import tqdm
 from PIL import Image
+import math
+Image.MAX_IMAGE_PIXELS = None
 
 # adapt as needed
 baseurl="https://histoapp.mevis.fraunhofer.de"
-patch_size = 8192
+patch_size = 16000
 project="project"
 image="frontend__29_29-Ki67__3_nonparametric.sqreg"
 level=4
@@ -30,15 +33,22 @@ def setupBigTiff(project, imageName, level):
     except KeyError:
         if metadata['status'] == "unauthenticated":
             raise Exception("Username or password seems to be wrong.")
-    extent = metadata["extent"]
+    extent = [math.ceil(d/(2**level)) for d in metadata["extent"]]
     voxelsize = [metadata["voxelsizes"][serverLevel]['x'], metadata["voxelsizes"][serverLevel]['y']]
     imagefile = pyvips.Image.black(extent[0],extent[1],bands=3)
     print("Downloading {} at resolution {}x{}...".format(imageName,extent[0],extent[1]))
     return imagefile, serverLevel, extent, voxelsize
 
 def getPatch(project, image, level, z, startPx, endPx, imagefile):
-    result = requests.get('{}/api/v1/projects/{}/images/{}/region/{}/start/{}/{}/{}/size/{}/{}'.format(baseurl, project, image, level, startPx[0], startPx[1], z, endPx[0]-startPx[0], endPx[1]-startPx[1]), auth = userCredentials)
+    url = '{}/api/v1/projects/{}/images/{}/region/{}/start/{}/{}/{}/size/{}/{}'.format(baseurl, project, image, level, startPx[0], startPx[1], z, endPx[0]-startPx[0], endPx[1]-startPx[1])
+    result = requests.get(url, auth = userCredentials)
+    try:
     image = Image.open(io.BytesIO(result.content))
+    except Exception as e:
+        print(url)
+        print(result)
+        print(result.content)
+        raise(e)
     imgNP =  np.array(image)
     image.close()
     w, h, channels = imgNP.shape
@@ -55,9 +65,9 @@ def main():
             startPx=(x,y)
             endPx=(extent[0] if x+patch_size > extent[0] else x+patch_size, extent[1] if y+patch_size > extent[1] else y+patch_size)
             if endPx[0] > extent[0]: endPx[0]
-            imagefile = getPatch(project, image, serverLevel, z, startPx, endPx, imagefile)
+            imagefile = getPatch(project, image, level, z, startPx, endPx, imagefile)
 
-    imagefile.tiffsave("{}_{}_{}.tif".format(image,level,z), xres=voxelsize[0], yres=voxelsize[1], tile=True, pyramid=True, compression="jpeg", bigtiff=True, rgbjpeg=True)
+    imagefile.tiffsave("{}_{}_{}.tif".format(image,level,z), xres=voxelsize[0], yres=voxelsize[1], tile=True, pyramid=True, compression="jpeg", bigtiff=True, rgbjpeg=False)
 
 if __name__ == "__main__":
     main()
